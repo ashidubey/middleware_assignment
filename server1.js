7edef39a4976b4fcb3a3b0214701af6cc5e64db9
Ashi Dubey <87648305+ashidubey@users.noreply.github.com>
2021-10-27 12:03:00 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -4,7 +4,7 @@ http.createServer(function (request, response) {
    
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   response.end('Hello World form server1\n');
+   response.end('Hello World from server1\n');
 }).listen(5001);
 
 console.log('Server running at http://127.0.0.1:5001/');
